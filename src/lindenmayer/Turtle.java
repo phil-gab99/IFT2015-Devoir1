a49c6b08fa520a7d52ed1f5303447d57bd155c7b
phil-gab99 <pgabriel999@hotmail.com>
2021-02-03 18:07:30 -0500

    Updated some files

---
+++ src/lindenmayer/Turtle.java
@@ -0,0 +1,55 @@
+package lindenmayer;
+
+import java.awt.geom.Point2D;
+
+/**
+ * The interface Turtle
+ * 
+ * @author Philippe Gabriel
+ * @version 1.0 2021-mm-dd
+ */
+
+public interface Turtle {
+    
+    /**
+     * The method draw draws a line of unit length
+     */
+     
+    void draw();
+    
+    /**
+     * The method move moves the turtle by unit length without drawing
+     */
+    
+    void move();
+    
+    /**
+     * The method turnR turns the turtle clockwise by unit angle
+     */
+    
+    void turnR();
+    
+    /**
+     * The method turnL turns the turtle counter-clockwise by unit angle
+     */
+     
+    void turnL();
+    
+    /**
+     * The method push saves the turtle state
+     */
+     
+    void push();
+    
+    /**
+     * The method pop recovers the saved turtle state
+     */
+     
+    void pop();
+    
+    /**
+     * The method stay 
+     * 
+     * 
+     */
+}
