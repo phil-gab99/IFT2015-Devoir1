7ab66b4a36e50f3eb5ace2a4abd2b0a31f6a88db
phil-gab99 <pgabriel999@hotmail.com>
2021-02-12 19:14:33 -0500

    Comment header for batch files

--- cleanResults.bat
+++ cleanResults.bat
@@ -1,2 +1,4 @@
+REM Script to delete all eps and pdf files in res directory after testing
+
 del res\*.eps
 del res\*.pdf
--- convertpdf.bat
+++ convertpdf.bat
@@ -1,1 +1,3 @@
+REM Script used to convert eps files in the res directory to pdf files
+
 FOR /F "tokens=1 delims=." %%a IN ('dir res /b ^| find ".eps"') DO epstopdf res/%%a.eps --output res/%%a.pdf
